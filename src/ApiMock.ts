ec0b37f90d8c0fa652a1e52ea18f263485dff7ad
Kaki Gao <kaki.gao@coatesgroup.com>
2023-09-10 21:08:48 +0800

    finish setup

---
+++ src/ApiMock.ts
@@ -0,0 +1,5 @@
+export const getPost = () => {
+	return new Promise(res => {
+		res([])
+	})
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { getPost } from "./ApiMock";
+
 export const App = () => {
+  const result = useQuery('getPost', getPost);
+  console.log("result", result);
   return <div>Learn React</div>;
 }
 
